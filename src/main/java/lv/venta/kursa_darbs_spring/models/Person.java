4919cc31f6bfdc59b5e87322b7a66cbd5ad66e60
s21dororebe <s21dororebe@venta.lv>
2023-06-02 22:06:09 +0300

    Remade to spring

--- src/main/java/lv/venta/kursa_darbs_spring/models/Person.java
+++ src/main/java/lv/venta/kursa_darbs_spring/models/Person.java
@@ -1,63 +1,36 @@
 package lv.venta.kursa_darbs_spring.models;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@MappedSuperclass
 public class Person {
-	private String name;
-	private String surname;
-	private String personCode;
+    @Column(name="idp")
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    @Setter(value= AccessLevel.NONE)
+    private long idp;
 
-	public String getName() {
-		return name;
-	}
-	
-	public void setName(String inputName) {
-		if(inputName != null && inputName.matches("[A-ZĒŪĪĀĻŅČŠŽ]{1}[a-zēīāūļžņš]+([ ][A-ZĒŪĪĀĻŅŠČŽ]{1}[a-zēīāūļžņš]+)?")) {
-			name = inputName;
-		}
-		else {
-			name = "Unknown";
-		}
-	}
-	
-	public String getSurname() {
-		return surname;
-	}
-	
-	public void setSurname(String inputSurname) {
-		if(inputSurname != null && inputSurname.matches("[A-ZĒŪĪĀĻŅČŠŽ]{1}[a-zēīāūļžņš]+([ ][A-ZĒŪĪĀĻŅŠČŽ]{1}[a-zēīāūļžņš]+)?")) {
-			surname = inputSurname;
-			}
-		else {
-			surname = "Unknown";
-			}
-	}
-	
-	public String getPersonCode() {
-		return personCode;
-	}
-	
-	public void setPersonCode(String inputPersonCode) {
-		if(inputPersonCode != null && inputPersonCode.matches("[0-9]{6}[-][0-9]{5}")) {
-			personCode = inputPersonCode;
-		}
-		else {
-			personCode = "000000-00000";
-		}
-	}
-	
-	public Person() {
-		setName("Unknown");
-		setSurname("Unknown");
-		setPersonCode("000000-00000");
-	}
-	
-	public Person(String name, String surname, String personCode) {
-		setName(name);
-		setSurname(surname);
-		setPersonCode(personCode);
-	}
+    @Column(name="name")
+    @Size(min=3, max=20)
+    @Pattern(regexp ="^[A-Z][a-zA-Z ]{0,39}$", message = "1.burtam jabut lielam, tikai latinu burti, max 40 simboli")
+    private String name;
 
-	public String toString() {
-		return "" + name + " " + surname + " " + personCode;
-	}
-	
+    @Column(name="surname")
+    @Size(min=3, max=20)
+    @Pattern(regexp ="^[A-Z][a-zA-Z ]{0,39}$", message = "1.burtam jabut lielam, tikai latinu burti, max 40 simboli")
+    private String surname;
+
+
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
 }
--- src/main/java/lv/venta/kursa_darbs_spring/models/User.java
+++ src/main/java/lv/venta/kursa_darbs_spring/models/User.java
@@ -1,100 +1,40 @@
 package lv.venta.kursa_darbs_spring.models;
 
-import java.security.MessageDigest;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Pattern;
+import lombok.*;
 
+@Table(name = "user_table")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
 public class User extends Person {
+	//TODO what we do with password?
+	@Pattern(regexp="[A-Za-z0-9]{8,20}")
 	private String encodedPassword;
+
+	@Column(name="email")
+	@Pattern(regexp="[a-z]+[0-9]{2}[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+[a-z$]")
 	private String email;
+
+	@Column(name="username")
+	@Pattern(regexp="[a-z0-9.]{8,20}")
 	private String username;
-	
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String inputUsername) {
-		if(inputUsername != null && inputUsername.matches("[a-z0-9.]{8,20}")) {
-			username = inputUsername;
-		}
-		else {
-			username = "default.user";
-		}
-	}
-	
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String inputEmail) {
-		if(inputEmail != null && inputEmail.matches("[a-z]+[0-9]{2}[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+[a-z$]")) {
-			email = inputEmail;
-		}
-		else {
-			
-			email = "default.email";
-		}
+
+	public User(String email, String username) {
+		this.email = email;
+		this.username = username;
 	}
 
-	public String getEncodedPassword() {
-		return encodedPassword;
-	}
-	public void setEncodedPassword(String inputEncodedPassword) {
-		if(inputEncodedPassword != null && inputEncodedPassword.matches("[A-Za-z0-9]{8,20}")) {
-			try {
-				MessageDigest md = MessageDigest.getInstance("MD5");
-				md.update(inputEncodedPassword.getBytes());
-				encodedPassword = new String(md.digest());	
-			}
-			catch (Exception e) {
-				encodedPassword = "defaultPassword";
-				}
-			}
-		else {
-			encodedPassword = "defaultPassword";
-		}
-	}
-	
-	public User() {
-		super();
-		setUsername("default.user");
-		setEncodedPassword("defaultPassword");
-		setEmail("default.email");
-	}
-	public User(String name, String surname, String username, String password) {
-		super();
-		setUsername(username);
-		setEncodedPassword(password);
-		setEmail(email);
-	}
-	public String toString() {
-		return "" + super.toString() + " " + email + " " + username;
+	public User(String name, String surname, String email, String username) {
+		super(name, surname);
+		this.email = email;
+		this.username = username;
 	}
 
-/*	public void signUp() throws Exception {
-		//check if user with the same email or username exists
-		for(User temp : MainService.allUsers){
-			if(temp.getUsername().equals(username)){
-				throw new Exception("User with this username already exists");
-			} else if (temp.getEmail().equals(email)){
-				throw new Exception("User with this email already exists");
-			}
-		}
-
-		if(username == null) {
-			throw (new Exception("Username is required"));
-		} else if(encodedPassword == null) {
-			throw (new Exception("Password is required"));
-		} else if(email == null) {
-			throw (new Exception("Email is required"));
-		} else createUser(username, encodedPassword, email);
-	}
-	public boolean login() {
-		for(User temp: MainService.allUsers) {
-			if(temp.getUsername().equals(username)
-					&& temp.getEncodedPassword().equals(encodedPassword)) {
-				return true;
-			}
-		}
-		return false;
 	//TODO bookABook()
 	//TODO ExtendExpiringDate()
-	}*/
 
 }
