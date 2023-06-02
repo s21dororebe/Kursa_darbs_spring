5ab7d0dc42c0aa8ba198ef038f865a6e6f74f700
s21dororebe <s21dororebe@venta.lv>
2023-06-02 22:12:49 +0300

    Edit: book remake in progress

--- src/main/java/lv/venta/kursa_darbs_spring/models/Book.java
+++ src/main/java/lv/venta/kursa_darbs_spring/models/Book.java
@@ -1,18 +1,30 @@
 package lv.venta.kursa_darbs_spring.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 import java.time.LocalDate;
 
+@Table(name = "book_table")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
 public class Book {
 	private long generatedId;
 	private static long idCounter = 0;
 	private String title;
-	private model.Author author;
+	private Author author;
 	private BookGenre genre;
 	private String description;
 	private int quantity = 0;
 	private LocalDate writingYear;
 	
-	// Constructors
 	public Book(String title, model.Author author, BookGenre genre, String description, int quantity, LocalDate writingYear) throws Exception {
         setGeneratedId();
 		setTitle(title);
@@ -22,54 +34,35 @@ public class Book {
 		setQuantity(quantity);
 		setWritingYear(writingYear);
     }
-	// Setters and Getters
-	
-	public long getGeneratedId() {
-		return generatedId;
-	}
+
 	public void setGeneratedId() {
 		this.generatedId = idCounter++;
 	}
 
-	public String getTitle() {
-        return title;
-    }
     public void setTitle(String title) throws Exception {
         if(title.length() > 0 && title.length() < 100){
             this.title = title;
         } else throw new Exception("Title cannot be empty");
     }
     
-    public model.Author getAuthor() {
-    	return author;
-    }
-    public void setAuthor(model.Author author) throws Exception {
+    public void setAuthor(Author author) throws Exception {
         if(author != null)
             this.author = author;
         else throw new Exception("Invalid input author");
     }
     
-    public BookGenre getGenre() {
-        return genre;
-    }
     public void setGenre(BookGenre genre) throws Exception {
         if(genre != null)
             this.genre = genre;
         else throw new Exception("Invalid input genre");
     }
 
-    public String getDescription() {
-        return description;
-    }
     public void setDescription(String description) throws Exception {
         if(description.length() > 0 && description.length() < 1000){
             this.description = description;
         } else throw new Exception("Description cannot be empty");
     }
     
-    public int getQuantity() {
-    	return quantity;
-    }
     public void setQuantity(int quantity) throws Exception {
     	if (quantity <= 99 && quantity > 0) {
             this.quantity = quantity;
@@ -78,9 +71,6 @@ public class Book {
         }
     }
 
-    public LocalDate getWritingYear() {
-        return writingYear;
-    }
     public void setWritingYear(LocalDate writingYear) throws Exception {
     	if (writingYear.isAfter(LocalDate.now())) {
             throw new Exception("Writing year cannot be in the future");
@@ -88,17 +78,7 @@ public class Book {
         this.writingYear = writingYear;
     }
 
-    @Override
-    public String toString() {
-        return "Book{" +
-                " title='" + title + '\'' +
-                ", author='" + author + '\'' +
-                ", genre=" + genre +
-                ", description='" + description + '\'' +
-                ", quantity=" + quantity +
-                ", writingYear=" + writingYear +
-                '}';
-    }
+
     
     
 }
